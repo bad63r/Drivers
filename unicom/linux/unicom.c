309c13a2618abdfe8c3db657a7f0cd48fcef221c
bad63r <bad63r@gmail.com>
2017-08-02 23:03:21 +0200

    projekat

---
+++ unicom/linux/unicom.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");	
+
+int unicom_init(void){
+
+	printk("<1>,Importing unicom driver to kernel...\n");
+
+return 0;
+}
+
+int unicom_exit(void){
+
+	printk("<1>,driver is disabled.\n");
+
+return 0;
+}
+
+module_init(unicom_init);
+module_exit(unicom_exit);
