2ef1e000afd022c3e545cfeaa4fbe8d41da7d972
Goran Mekić <meka@tilda.center>
2017-08-05 15:51:19 +0200

    Unify style

--- unicom/linux/unicom.c
+++ unicom/linux/unicom.c
@@ -1,21 +1,17 @@
 #include <linux/init.h>
-
 #include <linux/module.h>
 
-MODULE_LICENSE("GPL");	
 
-int unicom_init(void){
+MODULE_LICENSE("GPL");
 
+int unicom_init(void) {
 	printk("<1>,Importing unicom driver to kernel...\n");
-
-return 0;
+	return 0;
 }
 
-int unicom_exit(void){
-
+int unicom_exit(void) {
 	printk("<1>,driver is disabled.\n");
-
-return 0;
+	return 0;
 }
 
 module_init(unicom_init);
