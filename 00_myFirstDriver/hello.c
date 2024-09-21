3c4ae1439973ab07f196cb3fc5afe8107b0b2828
bad63r <bad63r@gmail.com>
2024-09-21 15:04:11 +0200

    adding comments

--- 00_myFirstDriver/hello.c
+++ 00_myFirstDriver/hello.c
@@ -1,19 +1,29 @@
-#include <linux/init.h>
-#include <linux/module.h>
-MODULE_LICENSE("Dual BDS/GPL");
+#include <linux/module.h> /* #include dynamic_debug inserts printk() implicitly;
+							 module_init(), module_exit()*/
+#include <linux/init.h> /* __init, __exit */
 
-static int __init hello_init(void){
-	printk("<1> My first driver is working \n");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("simple hello world module");
+MODULE_AUTHOR("bad63r");
 
-return 0;
+/* 
+** @brief This function is executed when module is inserted into kernel.
+*/
+static int __init driverInit(void)
+{
+	printk(KERN_INFO "hello there \n");
+	return 0;
 }
 
-static int __exit hello_exit(void){
-	printk("<1 >Leaving my first driver\n");
-	
-return 0;
+/* 
+** @brief This function is executed when module is removed from the kernel.
+*/
+static void __exit driverExit(void)
+{
+	printk(KERN_INFO "cya later, aligator! \n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
 
+
+module_init(driverInit);
+module_exit(driverExit);
